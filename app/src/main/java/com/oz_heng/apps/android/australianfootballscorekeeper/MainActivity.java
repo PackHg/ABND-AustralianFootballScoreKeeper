0b27704f462788bbb747d19ff360213a4e6f2898
Pack Heng <pack@oz-heng.com>
2017-08-10 17:05:15 +1000

    Used SharedPreferences to save the teams' scores as persistent data  On branch master  Changes to be committed: 	modified:   .idea/misc.xml 	modified:   app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/MainActivity.java

--- app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/MainActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/MainActivity.java
@@ -1,5 +1,6 @@
 package com.oz_heng.apps.android.australianfootballscorekeeper;
 
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
@@ -15,10 +16,8 @@ public class MainActivity extends AppCompatActivity {
     // Variables containing Teams A and B scores
     int teamAGoalScore = 0;
     int teamABehindScore = 0;
-    int teamATotalScore = 0;
     int teamBGoalScore = 0;
     int teamBBehindScore = 0;
-    int teamBTotalScore = 0;
 
     // Teams A & B score TextViews
     TextView teamAGoalScoreTextView;
@@ -28,9 +27,17 @@ public class MainActivity extends AppCompatActivity {
     TextView teamBBehindScoreTextView;
     TextView teamBTotalScoreTextView;
 
-    FragmentManager fragmentManager = getSupportFragmentManager();
-    final String fragmentTag = "Scoring Rules Dialog Fragment";
+    // Keys to save the scores if this activity is going to be stopped.
+    final static String KEY_TEAM_A_GOAL_SCORE = "teamAGoalScore";
+    final static String KEY_TEAM_A_BEHIND_SCORE = "teamABehindScore";
+    final static String KEY_TEAM_B_GOAL_SCORE = "teamBGoalScore";
+    final static String KEY_TEAM_B_BEHIND_SCORE = "teamBBehindScore";
 
+    // Preferences name used to save the teams scores.
+    final static String TEAMS_SCORES = "com.oz_heng.apps.android.australianfootballscorekeeper.TeamsScores";
+
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    final static String SCORING_RULES_DIALOG_FRAGMENT_TAG = "com.oz_heng.apps.android.australianfootballscorekeeper.ScoringRulesDialogFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,13 +47,34 @@ public class MainActivity extends AppCompatActivity {
         // Set UI in portrait mode.
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        // Restore the saved scores.
+        SharedPreferences teamsScoresSP = getSharedPreferences(TEAMS_SCORES, 0);
+        if (teamsScoresSP != null) {
+            teamAGoalScore = teamsScoresSP.getInt(KEY_TEAM_A_GOAL_SCORE, teamAGoalScore);
+            teamABehindScore = teamsScoresSP.getInt(KEY_TEAM_A_BEHIND_SCORE, teamABehindScore);
+            teamBGoalScore = teamsScoresSP.getInt(KEY_TEAM_B_GOAL_SCORE, teamBGoalScore);
+            teamBBehindScore = teamsScoresSP.getInt(KEY_TEAM_B_BEHIND_SCORE, teamBBehindScore);
+        }
+
+        // Identify the TextViews.
+        teamAGoalScoreTextView = (TextView) findViewById(R.id.team_a_goal_score);
+        teamABehindScoreTextView = (TextView) findViewById(R.id.team_a_behind_score);
         teamATotalScoreTextView= (TextView) findViewById(R.id.team_a_total_score);
+        teamBGoalScoreTextView = (TextView) findViewById(R.id.team_b_goal_score);
+        teamBBehindScoreTextView = (TextView) findViewById(R.id.team_b_behind_score);
         teamBTotalScoreTextView= (TextView) findViewById(R.id.team_b_total_score);
+
+        // Display the teams scores.
+        teamAGoalScoreTextView.setText(String.valueOf(teamAGoalScore));
+        teamABehindScoreTextView.setText(String.valueOf(teamABehindScore));
+        teamBGoalScoreTextView.setText(String.valueOf(teamBGoalScore));
+        teamBBehindScoreTextView.setText(String.valueOf(teamBBehindScore));
+        updaTeamATotalScore();
+        updaTeamBTotalScore();
 
         /* If Team A Goal Button is clicked, add 6 points to Team A Goal score TextView
            and update Team A total score TextView.
          */
-        teamAGoalScoreTextView = (TextView) findViewById(R.id.team_a_goal_score);
         Button teamAGoalButton = (Button) findViewById(R.id.team_a_goal);
         teamAGoalButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
         /* If Team A Behind Button is clicked, add 1 point to Team A Behind score TextView
            and update Team A total score TextView.
          */
-        teamABehindScoreTextView = (TextView) findViewById(R.id.team_a_behind_score);
         Button teamABehindButton = (Button) findViewById(R.id.team_a_behind);
         teamABehindButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +101,6 @@ public class MainActivity extends AppCompatActivity {
         /* If Team B Goal Button is clicked, add 6 points to Team B Goal score TextView
            and update Team B total score TextView.
          */
-        teamBGoalScoreTextView = (TextView) findViewById(R.id.team_b_goal_score);
         Button teamBGoalButton = (Button) findViewById(R.id.team_b_goal);
         teamBGoalButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -88,7 +114,6 @@ public class MainActivity extends AppCompatActivity {
         /* If Team B Behind Button is clicked, add 1 point to Team B Behind score TextView
            and update Team B total score TextView.
          */
-        teamBBehindScoreTextView = (TextView) findViewById(R.id.team_b_behind_score);
         Button teamBBehindButton = (Button) findViewById(R.id.team_b_behind);
         teamBBehindButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -120,35 +145,46 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.menu_scoring_rules:
                 ScoringRulesDialog scoringRulesDialog = new ScoringRulesDialog();
-                scoringRulesDialog.show(fragmentManager, fragmentTag);
+                scoringRulesDialog.show(fragmentManager, SCORING_RULES_DIALOG_FRAGMENT_TAG);
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
     /**
+     * Save the scores if this activity is going to be stopped.
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences teamsScoresSP = getSharedPreferences(TEAMS_SCORES, 0);
+        SharedPreferences.Editor editor = teamsScoresSP.edit();
+        editor.putInt(KEY_TEAM_A_GOAL_SCORE, teamAGoalScore);
+        editor.putInt(KEY_TEAM_A_BEHIND_SCORE, teamABehindScore);
+        editor.putInt(KEY_TEAM_B_GOAL_SCORE, teamBGoalScore);
+        editor.putInt(KEY_TEAM_B_BEHIND_SCORE, teamBBehindScore);
+        editor.apply();
+    }
+
+    /**
      * Update Team A total score
      */
     private void updaTeamATotalScore() {
-        teamATotalScore = teamAGoalScore + teamABehindScore;
-        teamATotalScoreTextView.setText(String.valueOf(teamATotalScore));
+        teamATotalScoreTextView.setText(String.valueOf(teamAGoalScore + teamABehindScore));
     }
 
     /**
      * Update Team B total score
      */
     private void updaTeamBTotalScore() {
-        teamBTotalScore = teamBGoalScore + teamBBehindScore;
-        teamBTotalScoreTextView.setText(String.valueOf(teamBTotalScore));
+        teamBTotalScoreTextView.setText(String.valueOf(teamBGoalScore + teamBBehindScore));
     }
 
     private void resetScores() {
         teamAGoalScore = 0;
         teamABehindScore = 0;
-        teamATotalScore = 0;
         teamBGoalScore = 0;
         teamBBehindScore = 0;
-        teamBTotalScore = 0;
 
         teamAGoalScoreTextView.setText(String.valueOf(0));
         teamABehindScoreTextView.setText(String.valueOf(0));
