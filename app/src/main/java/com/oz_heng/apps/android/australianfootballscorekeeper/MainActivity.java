8fa3f39b73ed06551c1428c5b1ea05dfc646ad61
Pack Heng <pack@oz-heng.com>
2017-08-12 09:15:40 +1000

    Renamed some variables and methods so they're better readable (following a suggestion of the project reviewer). Cleaned up the DialogFrament implementation.  Changes to be committed: 	modified:   app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/MainActivity.java 	modified:   app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/ScoringRulesDialog.java 	modified:   app/src/main/res/layout/dialog_scoring_rules.xml

--- app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/MainActivity.java
+++ app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/MainActivity.java
@@ -22,24 +22,24 @@ public class MainActivity extends AppCompatActivity {
     final FragmentManager fragmentManager = getSupportFragmentManager();
 
     // Keys to save the scores if this activity is going to be stopped.
-    private final static String KEY_TEAM_A_GOAL_SCORE = "teamAGoalScore";
-    private final static String KEY_TEAM_A_BEHIND_SCORE = "teamABehindScore";
-    private final static String KEY_TEAM_B_GOAL_SCORE = "teamBGoalScore";
-    private final static String KEY_TEAM_B_BEHIND_SCORE = "teamBBehindScore";
+    private final static String KEY_TEAM_A_GOAL_SCORE = "teamA_GoalScore";
+    private final static String KEY_TEAM_A_BEHIND_SCORE = "teamA_BehindScore";
+    private final static String KEY_TEAM_B_GOAL_SCORE = "teamB_GoalScore";
+    private final static String KEY_TEAM_B_BEHIND_SCORE = "teamB_BehindScore";
 
     // Variables containing Teams A and B scores
-    private int teamAGoalScore = 0;
-    private int teamABehindScore = 0;
-    private int teamBGoalScore = 0;
-    private int teamBBehindScore = 0;
+    private int teamA_GoalScore = 0;
+    private int teamA_BehindScore = 0;
+    private int teamB_GoalScore = 0;
+    private int teamB_BehindScore = 0;
 
     // Teams A & B score TextViews
-    private TextView teamAGoalScoreTextView;
-    private TextView teamABehindScoreTextView;
-    private TextView teamATotalScoreTextView;
-    private TextView teamBGoalScoreTextView;
-    private TextView teamBBehindScoreTextView;
-    private TextView teamBTotalScoreTextView;
+    private TextView teamA_GoalScoreTextView;
+    private TextView teamA_BehindScoreTextView;
+    private TextView teamA_TotalScoreTextView;
+    private TextView teamB_GoalScoreTextView;
+    private TextView teamB_BehindScoreTextView;
+    private TextView teamB_TotalScoreTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,77 +52,77 @@ public class MainActivity extends AppCompatActivity {
         // Restore the saved scores.
         SharedPreferences teamsScoresSP = getSharedPreferences(TEAMS_SCORES, 0);
         if (teamsScoresSP != null) {
-            teamAGoalScore = teamsScoresSP.getInt(KEY_TEAM_A_GOAL_SCORE, teamAGoalScore);
-            teamABehindScore = teamsScoresSP.getInt(KEY_TEAM_A_BEHIND_SCORE, teamABehindScore);
-            teamBGoalScore = teamsScoresSP.getInt(KEY_TEAM_B_GOAL_SCORE, teamBGoalScore);
-            teamBBehindScore = teamsScoresSP.getInt(KEY_TEAM_B_BEHIND_SCORE, teamBBehindScore);
+            teamA_GoalScore = teamsScoresSP.getInt(KEY_TEAM_A_GOAL_SCORE, teamA_GoalScore);
+            teamA_BehindScore = teamsScoresSP.getInt(KEY_TEAM_A_BEHIND_SCORE, teamA_BehindScore);
+            teamB_GoalScore = teamsScoresSP.getInt(KEY_TEAM_B_GOAL_SCORE, teamB_GoalScore);
+            teamB_BehindScore = teamsScoresSP.getInt(KEY_TEAM_B_BEHIND_SCORE, teamB_BehindScore);
         }
 
         // Identify the TextViews.
-        teamAGoalScoreTextView = (TextView) findViewById(R.id.team_a_goal_score);
-        teamABehindScoreTextView = (TextView) findViewById(R.id.team_a_behind_score);
-        teamATotalScoreTextView = (TextView) findViewById(R.id.team_a_total_score);
-        teamBGoalScoreTextView = (TextView) findViewById(R.id.team_b_goal_score);
-        teamBBehindScoreTextView = (TextView) findViewById(R.id.team_b_behind_score);
-        teamBTotalScoreTextView = (TextView) findViewById(R.id.team_b_total_score);
+        teamA_GoalScoreTextView = (TextView) findViewById(R.id.team_a_goal_score);
+        teamA_BehindScoreTextView = (TextView) findViewById(R.id.team_a_behind_score);
+        teamA_TotalScoreTextView = (TextView) findViewById(R.id.team_a_total_score);
+        teamB_GoalScoreTextView = (TextView) findViewById(R.id.team_b_goal_score);
+        teamB_BehindScoreTextView = (TextView) findViewById(R.id.team_b_behind_score);
+        teamB_TotalScoreTextView = (TextView) findViewById(R.id.team_b_total_score);
 
         // Display the teams scores.
-        teamAGoalScoreTextView.setText(String.valueOf(teamAGoalScore));
-        teamABehindScoreTextView.setText(String.valueOf(teamABehindScore));
-        teamBGoalScoreTextView.setText(String.valueOf(teamBGoalScore));
-        teamBBehindScoreTextView.setText(String.valueOf(teamBBehindScore));
-        updateTeamATotalScore();
-        updapteTeamBTotalScore();
+        teamA_GoalScoreTextView.setText(String.valueOf(teamA_GoalScore));
+        teamA_BehindScoreTextView.setText(String.valueOf(teamA_BehindScore));
+        teamB_GoalScoreTextView.setText(String.valueOf(teamB_GoalScore));
+        teamB_BehindScoreTextView.setText(String.valueOf(teamB_BehindScore));
+        updateTeamA_TotalScore();
+        updapteTeamB_TotalScore();
 
         /* If Team A Goal Button is clicked, add 6 points to Team A Goal score TextView
            and update Team A total score TextView.
          */
-        Button teamAGoalButton = (Button) findViewById(R.id.team_a_goal);
-        teamAGoalButton.setOnClickListener(new View.OnClickListener() {
+        Button teamA_GoalButton = (Button) findViewById(R.id.team_a_goal);
+        teamA_GoalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                teamAGoalScore += 6;
-                teamAGoalScoreTextView.setText(String.valueOf(teamAGoalScore));
-                updateTeamATotalScore();
+                teamA_GoalScore += 6;
+                teamA_GoalScoreTextView.setText(String.valueOf(teamA_GoalScore));
+                updateTeamA_TotalScore();
             }
         });
 
         /* If Team A Behind Button is clicked, add 1 point to Team A Behind score TextView
            and update Team A total score TextView.
          */
-        Button teamABehindButton = (Button) findViewById(R.id.team_a_behind);
-        teamABehindButton.setOnClickListener(new View.OnClickListener() {
+        Button teamA_BehindButton = (Button) findViewById(R.id.team_a_behind);
+        teamA_BehindButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                teamABehindScore += 1;
-                teamABehindScoreTextView.setText(String.valueOf(teamABehindScore));
-                updateTeamATotalScore();
+                teamA_BehindScore += 1;
+                teamA_BehindScoreTextView.setText(String.valueOf(teamA_BehindScore));
+                updateTeamA_TotalScore();
             }
         });
 
         /* If Team B Goal Button is clicked, add 6 points to Team B Goal score TextView
            and update Team B total score TextView.
          */
-        Button teamBGoalButton = (Button) findViewById(R.id.team_b_goal);
-        teamBGoalButton.setOnClickListener(new View.OnClickListener() {
+        Button teamB_GoalButton = (Button) findViewById(R.id.team_b_goal);
+        teamB_GoalButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                teamBGoalScore += 6;
-                teamBGoalScoreTextView.setText(String.valueOf(teamBGoalScore));
-                updapteTeamBTotalScore();
+                teamB_GoalScore += 6;
+                teamB_GoalScoreTextView.setText(String.valueOf(teamB_GoalScore));
+                updapteTeamB_TotalScore();
             }
         });
 
         /* If Team B Behind Button is clicked, add 1 point to Team B Behind score TextView
            and update Team B total score TextView.
          */
-        Button teamBBehindButton = (Button) findViewById(R.id.team_b_behind);
-        teamBBehindButton.setOnClickListener(new View.OnClickListener() {
+        Button teamB_BehindButton = (Button) findViewById(R.id.team_b_behind);
+        teamB_BehindButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                teamBBehindScore += 1;
-                teamBBehindScoreTextView.setText(String.valueOf(teamBBehindScore));
-                updapteTeamBTotalScore();
+                teamB_BehindScore += 1;
+                teamB_BehindScoreTextView.setText(String.valueOf(teamB_BehindScore));
+                updapteTeamB_TotalScore();
             }
         });
 
@@ -161,39 +161,39 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         SharedPreferences teamsScoresSP = getSharedPreferences(TEAMS_SCORES, 0);
         SharedPreferences.Editor editor = teamsScoresSP.edit();
-        editor.putInt(KEY_TEAM_A_GOAL_SCORE, teamAGoalScore);
-        editor.putInt(KEY_TEAM_A_BEHIND_SCORE, teamABehindScore);
-        editor.putInt(KEY_TEAM_B_GOAL_SCORE, teamBGoalScore);
-        editor.putInt(KEY_TEAM_B_BEHIND_SCORE, teamBBehindScore);
+        editor.putInt(KEY_TEAM_A_GOAL_SCORE, teamA_GoalScore);
+        editor.putInt(KEY_TEAM_A_BEHIND_SCORE, teamA_BehindScore);
+        editor.putInt(KEY_TEAM_B_GOAL_SCORE, teamB_GoalScore);
+        editor.putInt(KEY_TEAM_B_BEHIND_SCORE, teamB_BehindScore);
         editor.apply();
     }
 
     /**
      * Update Team A total score
      */
-    private void updateTeamATotalScore() {
-        teamATotalScoreTextView.setText(String.valueOf(teamAGoalScore + teamABehindScore));
+    private void updateTeamA_TotalScore() {
+        teamA_TotalScoreTextView.setText(String.valueOf(teamA_GoalScore + teamA_BehindScore));
     }
 
     /**
      * Update Team B total score
      */
-    private void updapteTeamBTotalScore() {
-        teamBTotalScoreTextView.setText(String.valueOf(teamBGoalScore + teamBBehindScore));
+    private void updapteTeamB_TotalScore() {
+        teamB_TotalScoreTextView.setText(String.valueOf(teamB_GoalScore + teamB_BehindScore));
     }
 
     private void resetScores() {
-        teamAGoalScore = 0;
-        teamABehindScore = 0;
-        teamBGoalScore = 0;
-        teamBBehindScore = 0;
+        teamA_GoalScore = 0;
+        teamA_BehindScore = 0;
+        teamB_GoalScore = 0;
+        teamB_BehindScore = 0;
 
-        teamAGoalScoreTextView.setText(String.valueOf(0));
-        teamABehindScoreTextView.setText(String.valueOf(0));
-        teamATotalScoreTextView.setText(String.valueOf(0));
-        teamBGoalScoreTextView.setText(String.valueOf(0));
-        teamBBehindScoreTextView.setText(String.valueOf(0));
-        teamBTotalScoreTextView.setText(String.valueOf(0));
+        teamA_GoalScoreTextView.setText(String.valueOf(0));
+        teamA_BehindScoreTextView.setText(String.valueOf(0));
+        teamA_TotalScoreTextView.setText(String.valueOf(0));
+        teamB_GoalScoreTextView.setText(String.valueOf(0));
+        teamB_BehindScoreTextView.setText(String.valueOf(0));
+        teamB_TotalScoreTextView.setText(String.valueOf(0));
     }
 }
 
--- app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/ScoringRulesDialog.java
+++ app/src/main/java/com/oz_heng/apps/android/australianfootballscorekeeper/ScoringRulesDialog.java
@@ -14,12 +14,12 @@ import android.widget.Button;
  */
 
 public class ScoringRulesDialog extends DialogFragment {
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.dialog_scoring_rules, container, false);
-        getDialog().setTitle(R.string.oz_rules_football);
 
         Button ok = (Button) rootView.findViewById(R.id.button_ok);
         ok.setOnClickListener(new View.OnClickListener() {
@@ -32,29 +32,4 @@ public class ScoringRulesDialog extends DialogFragment {
 
         return rootView;
     }
-
-    //    @NonNull
-//    @Override
-//    public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//
-//        // Get the layout inflater
-//        LayoutInflater inflater = getActivity().getLayoutInflater();
-//
-//        // Inflate and set the layout for the dialog
-//        // Pass null as the parent view because its going in the dialog layout
-//        builder.setView(inflater.inflate(R.layout.dialog_scoring_rules, null))
-//            .setTitle(R.string.oz_rules_football)
-//            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialogInterface, int i) {
-//                    if (dialogInterface != null) {
-//                        dialogInterface.dismiss();
-//                    }
-//                }
-//            });
-//
-//        // Create the AlertDialog object and return it
-//        return builder.create();
-//    }
 }
